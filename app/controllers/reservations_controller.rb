d1ec084d579f873fab159147a71073de9d092272
“bellaaaaaaaa” <gbayabos21@gmail.com>
2018-11-03 20:50:36 +0800

    added date validation for reserving listings

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -4,8 +4,27 @@ class ReservationsController < ApplicationController
         @reservation = Reservation.new(reservation_params)
         @reservation.user_id = current_user.id
         @reservation.listing_id = @listing.id
-        @reservation.save
-        redirect_to root_url
+
+        current_reservation_dates = [@reservation.check_in, @reservation.check_out] # Check in/out dates chosed by user for listing
+        current_listing_reservations = @reservation.listing.reservations # All reservations for current listing
+        current_listing_dates =[]
+
+        # Stores just the check_in/out dates for the current listing into current_listing_dates
+        current_listing_reservations.each do |i|
+            current_listing_dates << i.check_in
+            current_listing_dates << i.check_out
+        end
+
+        # Checks for overlap of dates between dates chosen by user and already existing reservations.
+        both =[current_reservation_dates, current_listing_dates]
+        both.inject(:&)
+        if both.inject(:&).length != 0
+            redirect_to root_url, notice: "Sorry this listing is already booked on those dates, please select different checkin/out dates."
+        else
+            @reservation.save
+            redirect_to root_url
+        end
+            
     end
 
     private 
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,30 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :listing
+  # validate :no_reservation_overlap
+
+#   def confirm_available
+#     # Check if dates overlap
+#       # If they do, add an error message
+#       # self => reservation being checked right now
+#         reservations = self.listing.reservations # resturns a list of reservations
+#         for reservation in reservations do        
+#   end
+
+# end
 end
+# scope :overlapping, ->(period_start, period_end) do
+#    where "((check_in <= ?) and (check_out >= ?))", period_end, period_start
+# end
+
+# private
+
+# def no_reservation_overlap
+#  if (Reservation.overlapping(check_in, check_out).any?)
+#     errors.add(:date_end, 'Your reservation overlaps another one. Please choose a different check in/out date.')
+#  end
+# end
+
+# reservations.each do |reservation|
+#   reservation.check_in
+# end
